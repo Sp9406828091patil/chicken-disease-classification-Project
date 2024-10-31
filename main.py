8d773d2a43bcb48b2b54b8578518b67df7fbb062
DESKTOP-92M4CLL\Win10 <s06.patil@gmail.com>
2024-10-31 06:06:05 +0530

    prepare call back model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.cnnClassifier import logger
 from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data ingestion stage"
 try:
@@ -7,6 +8,17 @@ try:
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
     logger.info(f">>>>>>>> {STAGE_NAME} end >>>>>>>>>>")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Prepare baseline model"
+try:
+    logger.info(f">>>>>>>>>>>>>> {STAGE_NAME} started <<<<<<<<<<<<<")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
+    logger.info(f">>>>>>>>>>>>>> {STAGE_NAME} completed <<<<<<<<<<<<<")
 except Exception as e:
     logger.exception(e)
     raise e
